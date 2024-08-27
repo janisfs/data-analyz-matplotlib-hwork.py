2a0bc3c749acfc893f5fc935417de5214a62a511
janisfs <janisfs@mail.ru>
2024-08-27 21:55:41 +0300

    add new code

--- divdivanpars/divdivanpars/spiders/divDivanPars.py
+++ divdivanpars/divdivanpars/spiders/divDivanPars.py
@@ -3,8 +3,14 @@ import scrapy
 
 class DivDivanParsSpider(scrapy.Spider):
     name = "divDivanPars"
-    allowed_domains = ["divan.ru"]
-    start_urls = ["https://divan.ru"]
+    allowed_domains = ["divan.ru", "www.divan.ru"]  # Учет обеих форм записи сайта
+    start_urls = ["https://www.divan.ru/category/divany-i-kresla"]
 
     def parse(self, response):
-        pass
+        svets = response.css('div._Ud0k')  # Основной контейнер товаров
+        for svet in svets:
+            yield {
+                "name": svet.css("div.wYUX2 span::text").get(),  # Название товара
+                "price": svet.css("div.pY3d2 span::text").get(),  # Цена товара
+                "url": response.urljoin(svet.css("a").attrib["href"])  # Ссылка на товар
+            }
